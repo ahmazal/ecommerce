57959fb915544bdda7eabef11cd36f000b6327cd
ahmazal <rizalrahmdni47@gmail.com>
2025-10-26 13:48:47 +0700

    tinggal deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 3000,
+  },
   plugins: [react(),tailwindcss()],
 })
